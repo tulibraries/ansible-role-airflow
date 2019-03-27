4f475f190228482cef4e744a1fae320410fe9e7d
Christina Harlow <cmharlow@gmail.com>
2019-03-27 18:33:33 -0400

    adding passing tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,8 +9,6 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 TESTINFRA_HOSTS = AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-print(TESTINFRA_HOSTS)
-
 
 def test_hosts_file(host):
     """
@@ -74,8 +72,8 @@ def test_debian_ubuntu_prerequisites_packages(host, name, codenames):
     ('libxml2-devel', None),
     ('libxslt-devel', None),
     ('python2-pip', None),
-    ('python2-virtualenv', None),
-    ('python2-setuptools', None),
+    ('python-virtualenv', None),
+    ('python-setuptools', None),
     ('gcc', None)
 ])
 def test_centos_prerequisites_packages(host, name, codenames):
@@ -120,6 +118,20 @@ def test_airflow_services(host, name):
         assert service.is_enabled
 
 
+@pytest.mark.parametrize('library', [
+    ('apache-airflow'),
+    ('cryptography')
+])
+def test_pip_libraries(host, library):
+    """
+    Test pip libraries installed
+    """
+
+    pip_path = "/var/lib/airflow/venv/bin/pip3.6"
+    libraries = host.pip_package.get_packages(pip_path=pip_path)
+    assert libraries.get(library)
+
+
 @pytest.mark.parametrize('path,path_type,user,group,mode', [
     ('/var/run/airflow', 'directory', 'airflow', 'airflow', 0o700),
     ('/var/lib/airflow/airflow', 'directory', 'airflow', 'airflow', 0o700),
@@ -156,3 +168,38 @@ def test_airflow_dags_paths(host, path):
 
     assert current_path.exists
     assert current_path.is_directory
+
+
+@pytest.mark.parametrize('path,path_type,user,group,mode', [
+    ('/var/log/airflow', 'directory', 'airflow', 'airflow', 0o700),
+    ('/var/run/airflow', 'directory', 'airflow', 'airflow', 0o700),
+    ('/etc/logrotate.d/airflow', 'file', 'root', 'root', 0o644)
+    ])
+def test_logging_paths(host, path, path_type, user, group, mode):
+    """
+    Tests Airflow logging & pid paths
+    """
+    current_path = host.file(path)
+
+    assert current_path.exists
+
+    if path_type == 'directory':
+        assert current_path.is_directory
+    elif path_type == 'file':
+        assert current_path.is_file
+    assert current_path.user == user
+    assert current_path.group == group
+    assert current_path.mode == mode
+
+
+@pytest.mark.parametrize('username,email,firstname,lastname,password', [
+    ('google_112545704984098245976', 'tul08567@temple.edu', 'Christina',
+     'Harlow', 'dummy-password')
+    ])
+def test_airflow_users(username, email, firstname, lastname, password):
+    """
+    Tests Airflow is creating Users
+    """
+
+    users = os.system('/var/lib/airflow/venv/bin/airfow list_users')
+    print(users)
