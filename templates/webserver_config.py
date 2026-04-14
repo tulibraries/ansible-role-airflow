5f22a5e5b59cb746d96d3808c17409d48c023df0
Jen <tuh42082@temple.edu>
2026-04-14 13:29:24 -0400

    Updates to oauth config (working in qa) (#139)

--- templates/webserver_config.py
+++ templates/webserver_config.py
@@ -1,5 +1,7 @@
 import os
 from flask_appbuilder.security.manager import AUTH_OAUTH
+from airflow.providers.fab.auth_manager.security_manager.override import FabAirflowSecurityManagerOverride
+from flask import current_app
 
 # ----------------------------------------------------
 # Basic Security
@@ -10,7 +12,6 @@ CSRF_ENABLED = True
 # Authentication
 # ----------------------------------------------------
 AUTH_TYPE = AUTH_OAUTH
-
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = "Viewer"
 
@@ -28,10 +29,12 @@ OAUTH_PROVIDERS = [
         "name": "google",
         "icon": "fa-google",
         "token_key": "access_token",
+        "userinfo_endpoint": "https://openidconnect.googleapis.com/v1/userinfo",
         "remote_app": {
             "client_id": GOOGLE_CLIENT_ID,
             "client_secret": GOOGLE_CLIENT_SECRET,
             "server_metadata_url": "https://accounts.google.com/.well-known/openid-configuration",
+            "api_base_url": "https://openidconnect.googleapis.com/v1/",
             "client_kwargs": {
                 "scope": "openid email profile"
             },
@@ -42,16 +45,17 @@ OAUTH_PROVIDERS = [
 # ----------------------------------------------------
 # Optional: restrict allowed domains
 # ----------------------------------------------------
-def oauth_user_info(provider, response=None):
-    if provider == "google":
-        email = response.get("email")
+class CustomSecurityManager(FabAirflowSecurityManagerOverride):
 
-        if not email or not email.endswith("@temple.edu"):
-            return None
+    def auth_user_oauth(self, userinfo):
+        email = userinfo.get("email")
 
-        return {
-            "username": email,
-            "email": email,
-            "first_name": response.get("given_name", ""),
-            "last_name": response.get("family_name", ""),
-        }
+        if email:
+            user = self.find_user(email=email)
+            if user:
+                return user
+
+        return super().auth_user_oauth(userinfo)
+
+
+SECURITY_MANAGER_CLASS = CustomSecurityManager
