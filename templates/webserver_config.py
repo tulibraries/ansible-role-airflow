737e80e5a6801ecb1d370512b16f4470068e6495
sensei100 <tuh42082@temple.edu>
2026-04-14 09:56:31 -0400

    Update google oauth settings

--- templates/webserver_config.py
+++ templates/webserver_config.py
@@ -31,13 +31,10 @@ OAUTH_PROVIDERS = [
         "remote_app": {
             "client_id": GOOGLE_CLIENT_ID,
             "client_secret": GOOGLE_CLIENT_SECRET,
-            "api_base_url": "https://www.googleapis.com/oauth2/v2/",
+            "server_metadata_url": "https://accounts.google.com/.well-known/openid-configuration",
             "client_kwargs": {
                 "scope": "openid email profile"
             },
-            "access_token_url": "https://accounts.google.com/o/oauth2/token",
-            "authorize_url": "https://accounts.google.com/o/oauth2/auth",
-            "request_token_url": None,
         },
     }
 ]
